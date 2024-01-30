a203f12b9218c23d55114ee897a3058e5918b0f6
Seung Park <seung.park@mongodb.com>
2024-01-30 15:21:54 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Reproduce Bug for Transformer Sharp`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
